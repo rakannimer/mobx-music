a324951b13dbe285bede9cc6b2aa0d77fc9b02ce
rakannimer <rakannimer@gmail.com>
2018-11-29 08:40:51 +0200

    chore: better demo

--- demo/index.ts
+++ demo/index.ts
@@ -1,10 +1,25 @@
-import { getInstruments } from "../src/";
+import { getInstruments, InstrumentName } from "../src/";
+
+const playNote = async (instrumentName: InstrumentName) => {
+  const { instruments } = await getInstruments([instrumentName]);
+  await instruments.get(instrumentName).play("A4", { duration: 1000 });
+};
 
 const main = async () => {
   console.log("Hello");
-  const { instruments } = await getInstruments(["accordion"]);
-  instruments.get("accordion").play("A4", { duration: 1000 });
+  const selectedInstrumentInput = document.getElementById(
+    "selected-instrument"
+  );
+
+  // console.log(selectedInstrumentInput);
+  selectedInstrumentInput.addEventListener("change", async ev => {
+    const selectedInstrumentElement = ev.currentTarget as HTMLSelectElement;
+    const newInstrumentName = selectedInstrumentElement.value as InstrumentName;
+    await playNote(newInstrumentName);
+  });
+  await playNote("accordion");
 };
+
 (async () => {
   await main();
 })();
